197b47db8c8ca1da7b62c68c8c25af1f320a5b1e
Michael Ketchel <mdk@rgnets.com>
2025-02-04 21:27:26 -0500

    Using new bus pattern, fairly bulletproofed

--- wlanpi_rxg_agent/lib/domain.py
+++ wlanpi_rxg_agent/lib/domain.py
@@ -1,15 +1,33 @@
 from enum import Enum
+import typing as t
+from dataclasses import dataclass
+
 class RxgAgentEvents(Enum):
     STARTUP_COMPLETE = "STARTUP_COMPLETE"   # All core modules relying on the bus should be up and listening when this is dispatched.
     SHUTDOWN_STARTED = "SHUTDOWN_STARTED"   # Signals modules to start teardown
     SHUTDOWN_COMPLETE = "SHUTDOWN_COMPLETE"
 
-
-import typing as t
 class Messages:
     class StartupComplete(t.NamedTuple):
         pass
     class ShutdownStarted(t.NamedTuple):
         pass
     class ShutdownComplete(t.NamedTuple):
+        pass
+    class AgentConfigUpdated(t.NamedTuple):
+        pass
+
+    @dataclass
+    class AgentConfigUpdate:
+        override_rxg: t.Optional[str] = None
+        fallback_rxg: t.Optional[str] = None
+        safe: bool = False # If the update should only succeed if the overrides match live servers
+
+
+    @dataclass
+    class Error:
+        msg: str
+        exc: t.Optional[Exception]
+
+    class MqttError(Error):
         pass
--- wlanpi_rxg_agent/lib/rxg_supplicant/domain.py
+++ wlanpi_rxg_agent/lib/rxg_supplicant/domain.py
@@ -1,6 +1,8 @@
 from enum import Enum
 import typing as t
+from dataclasses import dataclass
 
+from lib import domain as agent_domain
 
 PREFIX = "RXG_SUPPLICANT_"
 class RxgSupplicantEvents(Enum):
@@ -30,13 +32,46 @@ class Messages:
         pass
     class Certifying(t.NamedTuple):
         pass
-    class Certified(t.NamedTuple):
+
+    @dataclass
+    class Certified:
+        host: str
+        port: int
+        status: str
+        certificate: str
+        ca: str
+        ca_file: str
+        certificate_file: str
+        key_file: str
+        cert_reqs: int
+
+        def __eq__(self, other):
+            if not isinstance(other, self.__class__):
+                return NotImplemented
+            return all([
+                self.host == other.host,
+                self.port == other.port,
+                self.status == other.status,
+                self.certificate == other.certificate,
+                self.ca == other.ca,
+                self.ca_file == other.ca_file,
+                self.certificate_file == other.certificate_file,
+                self.key_file == other.key_file,
+                self.cert_reqs == other.cert_reqs,
+            ])
+
+    class NewCertifiedConnection(Certified):
         pass
+
+    class RestartInternalMqtt(Certified):
+        pass
+
     class CertificationFailed(t.NamedTuple):
         pass
     class Monitoring(t.NamedTuple):
         pass
     class NewServer(t.NamedTuple):
         pass
-    class Error(t.NamedTuple):
-        error: Exception
+
+    class Error(agent_domain.Messages.Error):
+        pass
