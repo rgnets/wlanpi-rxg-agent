6869440144e9cfb1b4e5d2c36d3714d54ed3968a
Michael Ketchel <mdk@rgnets.com>
2025-04-24 15:01:30 -0400

    Enhance logger for message bus to be able to show message payloads

--- wlanpi_rxg_agent/lib/event_bus/middleware/logger.py
+++ wlanpi_rxg_agent/lib/event_bus/middleware/logger.py
@@ -8,9 +8,11 @@ import logging
 
 
 class LoggingMiddlewareConfig(t.NamedTuple):
-    mgs_received_level: int = logging.DEBUG
-    mgs_succeeded_level: int = logging.DEBUG
-    mgs_failed_level: int = logging.ERROR
+    msg_received_level: int = logging.DEBUG
+    msg_succeeded_level: int = logging.DEBUG
+    msg_failed_level: int = logging.ERROR
+    include_msg_payload: bool = False
+
 
 
 def get_logger_middleware(
@@ -22,22 +24,33 @@ def get_logger_middleware(
     def logger_middleware(message: object, next_: t.Callable) -> object:
         message_type = type(message)
 
+        log_msg = f"Message received: ${message_type}"
+        if middleware_config.include_msg_payload:
+            log_msg += f" with payload: {message}"
+
         logger.log(
-            middleware_config.mgs_received_level, f"Message received: ${message_type}"
+            middleware_config.msg_received_level, log_msg
         )
 
         try:
             result = next_(message)
         except Exception as err:
+            log_msg = f"Message failed: ${message_type}"
+            if middleware_config.include_msg_payload:
+                log_msg += f" with payload: {message}"
             logger.log(
-                middleware_config.mgs_failed_level,
-                f"Message failed: ${message_type}",
+                middleware_config.msg_failed_level,
+                log_msg,
                 exc_info=True,
             )
             raise err
 
+        # log_msg = f"Message succeeded: ${message_type}"
+        # if middleware_config.include_msg_payload:
+        #     log_msg += f" with payload: {message}"
+
         logger.log(
-            middleware_config.mgs_succeeded_level, f"Message succeeded: ${message_type}"
+            middleware_config.msg_succeeded_level, f"Message succeeded: ${message_type}"
         )
 
         return result
