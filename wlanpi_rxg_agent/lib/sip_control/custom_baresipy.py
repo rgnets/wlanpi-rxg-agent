7cc4f3d3da49b4e0c991c1d4249183b85df967ff
Michael Ketchel <mdk@rgnets.com>
2025-07-02 16:14:34 -0400

    checkpoint before pivoting to async reimplementation of baresipy

--- wlanpi_rxg_agent/lib/sip_control/custom_baresipy.py
+++ wlanpi_rxg_agent/lib/sip_control/custom_baresipy.py
@@ -2,6 +2,7 @@ import asyncio
 import time
 from typing import Optional
 
+import event_emitter
 import pexpect
 from baresipy import BareSIP, LOG
 from responsive_voice import ResponsiveVoice
@@ -17,6 +18,7 @@ class CustomBaresipy(BareSIP):
         self.user = user
         self.pwd = pwd
         self.gateway = gateway
+        self.em = event_emitter.EventEmitter()
         if tts:
             self.tts = tts
         else:
@@ -45,6 +47,12 @@ class CustomBaresipy(BareSIP):
     #     LOG.info("Adding account: " + self.user)
     #     self.baresip.sendline("/uanew " + self._login)
 
+    def handle_incoming_call(self, number):
+        ''' Overrides default so it doesn't automatically handle calls.'''
+        LOG.info("Incoming call: " + number)
+        time.sleep(0.1)
+        self.do_command("b")
+
     def run(self):
         self.running = True
         while self.running:
@@ -57,31 +65,40 @@ class CustomBaresipy(BareSIP):
                         LOG.debug(out)
                     if "baresip is ready." in out:
                         self.handle_ready()
+                        self.em.emit("ready", raw=out, bs_instance=self)
                     elif "account: No SIP accounts found" in out:
                         self._handle_no_accounts()
+                        self.em.emit("no_accounts", raw=out, bs_instance=self)
                     elif "All 1 useragent registered successfully!" in out:
                         self.ready = True
                         self.handle_login_success()
+                        self.em.emit("login_success", raw=out, bs_instance=self)
                     elif "ua: SIP register failed:" in out or\
                             "401 Unauthorized" in out or \
                             "Register: Destination address required" in out or\
                             "Register: Connection timed out" in out:
                         self.handle_error(out)
+                        self.em.emit("error",error=out, raw=out, bs_instance=self)
                         self.handle_login_failure()
+                        self.em.emit("login_failure", raw=out, bs_instance=self)
                     elif "Incoming call from: " in out:
                         num = out.split("Incoming call from: ")[
                             1].split(" - (press 'a' to accept)")[0].strip()
                         self.current_call = num
                         self._call_status = "INCOMING"
+                        self.em.emit("call_status", status=self._call_status, raw=out, bs_instance=self)
                         self.handle_incoming_call(num)
+                        self.em.emit("incoming_call", num=num, raw=out, bs_instance=self)
                     elif "call: rejecting incoming call from " in out:
                         num = out.split("rejecting incoming call from ")[1].split(" ")[0].strip()
                         self.handle_call_rejected(num)
+                        self.em.emit("rejecting_call", num=num, raw=out, bs_instance=self)
                     elif "call: SIP Progress: 180 Ringing" in out:
                         self.handle_call_ringing()
                         status = "RINGING"
                         self.handle_call_status(status)
                         self._call_status = status
+                        self.em.emit("call_status", status=self._call_status, raw=out, bs_instance=self)
                     elif "call: connecting to " in out:
                         n = out.split("call: connecting to '")[1].split("'")[0]
                         self.current_call = n
@@ -89,26 +106,32 @@ class CustomBaresipy(BareSIP):
                         status = "OUTGOING"
                         self.handle_call_status(status)
                         self._call_status = status
+                        self.em.emit("call_status", status=self._call_status, raw=out, bs_instance=self)
                     elif "Call established:" in out:
 
                         status = "ESTABLISHED"
                         self.handle_call_status(status)
                         self._call_status = status
                         time.sleep(0.5)
+                        self.em.emit("call_status", status=self._call_status, raw=out, bs_instance=self)
                         self.handle_call_established()
+                        self.em.emit("call_established", raw=out, bs_instance=self)
                     elif "call: hold " in out:
                         n = out.split("call: hold ")[1]
                         status = "ON HOLD"
                         self.handle_call_status(status)
                         self._call_status = status
+                        self.em.emit("call_status", status=self._call_status, raw=out, bs_instance=self)
                     elif "Call with " in out and \
                             "terminated (duration: " in out:
                         status = "DISCONNECTED"
                         duration = out.split("terminated (duration: ")[1][:-1]
                         self.handle_call_status(status)
                         self._call_status = status
+                        self.em.emit("call_status", status=self._call_status, raw=out, bs_instance=self)
                         self.handle_call_timestamp(duration)
                         self.mic_muted = False
+                        self.em.emit("call_terminated", duration=duration, raw=out, bs_instance=self)
                     elif "call muted" in out:
                         self.mic_muted = True
                         self.handle_mic_muted()
@@ -120,16 +143,20 @@ class CustomBaresipy(BareSIP):
                         status = "DISCONNECTED"
                         self.handle_call_status(status)
                         self._call_status = status
+                        self.em.emit("call_status", status=self._call_status, raw=out, bs_instance=self)
                         self.handle_call_ended(reason)
                         self.mic_muted = False
+                        self.em.emit("session_closed",reason=reason, raw=out, bs_instance=self)
                     elif "(no active calls)" in out:
                         status = "DISCONNECTED"
                         self.handle_call_status(status)
                         self._call_status = status
+                        self.em.emit("call_status", status=self._call_status, raw=out, bs_instance=self)
                     elif "===== Call debug " in out:
                         status = out.split("(")[1].split(")")[0]
                         self.handle_call_status(status)
                         self._call_status = status
+                        self.em.emit("call_status", status=self._call_status, raw=out, bs_instance=self)
                     elif "--- List of active calls (1): ---" in \
                             self._prev_output:
                         if "ESTABLISHED" in out and self.current_call in out:
@@ -141,6 +168,7 @@ class CustomBaresipy(BareSIP):
                     elif "failed to set audio-source (No such device)" in out:
                         error = "failed to set audio-source (No such device)"
                         self.handle_error(error)
+                        self.em.emit("error", error=error, raw=out, bs_instance=self)
                     elif "EX=BareSip;" in out:
                         summary_data = {}
                         elements = out.split(";")
@@ -150,6 +178,7 @@ class CustomBaresipy(BareSIP):
                                 summary_data[key] = value
 
                         self.handle_rtcp_summary(summary_data)
+                        self.em.emit("rtcp_summary", summary=summary_data, raw=out, bs_instance=self)
 
                     self._prev_output = out
             except pexpect.exceptions.EOF:
@@ -162,6 +191,7 @@ class CustomBaresipy(BareSIP):
 
     async def async_wait_until_ready(self):
         while not self.ready:
+            LOG.debug("Waiting for baresip to be ready...")
             await asyncio.sleep(0.1)
             if self.abort:
                 return
--- wlanpi_rxg_agent/lib/sip_control/sip_test.py
+++ wlanpi_rxg_agent/lib/sip_control/sip_test.py
@@ -2,20 +2,20 @@ import asyncio
 import logging
 import time
 from typing import Optional
-
+import threading
 from lib.sip_control.custom_baresipy import CustomBaresipy
 from lib.sip_control.sip_control import SipInstance
 
 
-class SipTestBaresip(SipInstance):
+class SipTestBaresip:
 
     def __init__(self,
                  gateway:str,
                  user: str,
                  password: str,
-                 callee: str,
-                 post_connect: Optional[str] = None,
-                 call_timeout: Optional[int] = None,
+                 # callee: str,
+                 # post_connect: Optional[str] = None,
+                 # call_timeout: Optional[int] = None,
                  extra_login_args: Optional[str] = None,
                  debug: bool=False
                  ):
@@ -25,44 +25,76 @@ class SipTestBaresip(SipInstance):
         self._gateway = gateway
         self._user = user
         self._password = password
-        self.callee = callee
-        self.post_connect = post_connect
-        self._extra_login_args = f""
-        self.custom_baresip = self.build_custom_baresip_class()
-        self.baresip = self.custom_baresip(user = self._user, pwd=self._password, gateway=self._gateway, block=True, debug=debug)
-        # self.baresip = CustomBaresipy(user = self._user, pwd=self._password, gateway=self._gateway, block=True, debug=debug)
-        # self.baresip.start()
-        self.logger.info("Baresip started")
+        self._debug = debug
+        # self.callee = callee
+        # self.post_connect = post_connect
+        self._extra_login_args = extra_login_args
+        self.baresip = CustomBaresipy(user=self._user, pwd=self._password, gateway=self._gateway, block=False,
+                                                debug=self._debug)
+        self.logger.info("SipTestBaresip initialized")
 
 
-    def build_custom_baresip_class(self):
-        my_self = self
-        class CustomizedBaresipy(CustomBaresipy):
 
-            def handle_rtcp_summary(self, summary_data: dict):
-                my_self.logger.info(f"Received RTCP summary: {summary_data}")
+    async def execute(self, callee: str, post_connect: Optional[str] = None, call_timeout: Optional[int] = None):
+        self.logger.info(f"Executing test against {callee}")
 
-            def handle_call_established(self):
-                if my_self.post_connect:
-                    my_self.baresip.send_dtmf(my_self.post_connect)
-                my_self.baresip.speak("Your mom's a hoe.")
-                time.sleep(0.5)
-                my_self.baresip.hang()
-                my_self.baresip.quit()
+        loop = asyncio.get_running_loop()
 
-        return CustomizedBaresipy
+        in_progress = loop.create_future()
 
+        def handle_rtcp_summary( summary: dict, *_, **__):
+            self.logger.info(f"Received RTCP summary: {summary}")
 
-    async def execute(self):
-        self.logger.info(f"Executing test against {self.callee}")
-        self.baresip.call(self.callee)
-        # Dirty hack for now, don't keep this shit.
-        while self.baresip.running:
+        def handle_call_established(bs_instance:CustomBaresipy, *_, **__):
+            if post_connect:
+                bs_instance.send_dtmf(post_connect)
+            bs_instance.speak("Your mom's a hoe.")
             time.sleep(0.5)
+            bs_instance.hang()
+            # in_progress.set_result(True)
+            # self.baresip.quit()
+
+        def handle_call_ended_abnormally(bs_instance:CustomBaresipy, *_, **__):
+            self.logger.info("Call ended abnormally")
+            self.logger.debug((in_progress.done(), in_progress.cancelled()))
+            if not (in_progress.done() or in_progress.cancelled()):
+                in_progress.set_result(False)
+
+        def handle_call_ended_normally(bs_instance:CustomBaresipy, *_, **__):
+
+            print(f"(call ended) Current thread ID: {threading.get_ident()}")
+            self.logger.info("Call ended normally")
+            self.logger.debug((in_progress.done(), in_progress.cancelled()))
+            if not (in_progress.done() or in_progress.cancelled()):
+                in_progress.set_result(True)
+
+        self.baresip.em.on("rtcp_summary", handle_rtcp_summary)
+        self.baresip.em.on("call_established", handle_call_established)
+        self.baresip.em.on("session_closed", handle_call_ended_abnormally)
+        self.baresip.em.on("call_terminated", handle_call_ended_normally)
+        self.baresip.call(callee)
+
+        # Dirty hack for now, don't keep this shit.
+        # while self.baresip.running:
+        #     time.sleep(0.5)
+        self.logger.info("Waiting for call to end")
+
+        print(f"(waiting) Current thread ID: {threading.get_ident()}")
+        await in_progress
+        self.logger.info("Test completed")
         # self.baresip.quit()
 
 
-    def __del__(self):
+    async def __aenter__(self):
+        self.logger.info("Entering async context")
+        await self.baresip.async_wait_until_ready()
+        self.logger.info("Baresip is ready")
+        return self
+
+
+
+    async def __aexit__(self, exc_type, exc, tb):
+        self.logger.info("Exiting async context")
         try:
             self.baresip.quit()
         except:
@@ -70,12 +102,29 @@ class SipTestBaresip(SipInstance):
 
 
 
+    #
+    # def __del__(self):
+    #     try:
+    #         self.baresip.quit()
+    #     except:
+    #         self.logger.warning("self-cleanup error; baresip may already have been quit.", exc_info=True)
+
+
+
 if __name__ == "__main__":
 
     async def main():
-        sip_t = SipTestBaresip(gateway="192.168.7.15", user="pi2", password="pitest", callee="1101",
-                               post_connect="1234")
-        await sip_t.execute()
+        # sip_t = SipTestBaresip(gateway="192.168.7.15", user="pi2", password="pitest", callee="1101",
+        #                        post_connect="1234")
+        # await sip_t.execute()
+
+        print(f"(main) Current thread ID: {threading.get_ident()}")
+
+
+        async with SipTestBaresip(gateway="192.168.7.15", user="pi2", password="pitest") as sip_test:
+            print(f"(in context) Current thread ID: {threading.get_ident()}")
+            await sip_test.execute(callee="1101", post_connect="1234", call_timeout=10)
+            print("done!")
 
     asyncio.get_event_loop().run_until_complete(main())
 
