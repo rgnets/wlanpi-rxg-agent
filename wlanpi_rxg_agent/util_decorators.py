c7302c34a94f063b03cae0048d2f16efb2aa8e04
Michael Ketchel <mdk@rgnets.com>
2025-07-07 20:41:00 -0400

    Added some debug decorators

---
+++ wlanpi_rxg_agent/util_decorators.py
@@ -0,0 +1,18 @@
+
+import time
+
+def debug_print_execution_time(func):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+    return wrapper
+
+def debug_announce_execution(func):
+    def wrapper(*args, **kwargs):
+        print(f"Executing {func.__name__}")
+        result = func(*args, **kwargs)
+        return result
+    return wrapper
